26bc89674ee823a68cceb1a064c8eaabf613ae74
Prasad <banprasad21@gmail.com>
2022-08-17 00:06:38 +0530

    [Raghav] Add: perfect number within given range program

---
+++ LogicalProblems/PerfectNumber.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProblems
+{
+    internal class PerfectNumber
+    {
+        public static void GetPerfectnumber()
+        {
+            int n, i, sum, min, max;
+            Console.WriteLine("Enter Start point number");
+            min = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter End point number");
+            max = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Perfect number in provided range is:");
+            for (n = min; n <= max; n++)
+            {
+                i = 1;
+                sum = 0;
+                while (i < n)
+                {
+                    if (n % i == 0)
+
+                        sum += i;
+                    i++;
+
+
+                }
+                if (sum == n)
+                    Console.WriteLine("{0}", n);
+            }
+        }
+    }
+}
--- LogicalProblems/Program.cs
+++ LogicalProblems/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            FibonacciSeries.Fibonacci();
+            Console.WriteLine("Logical Programs");
+            Console.WriteLine("Enter a number to select problem");
+            Console.WriteLine("1. Fibonacci Series");
+            Console.WriteLine("2. Perfect Number");
+            int ip = Convert.ToInt32(Console.ReadLine());
+            switch (ip)
+            {
+                case 1:
+                    FibonacciSeries.Fibonacci();
+                    break;
+
+                case 2;
+                    PerfectNumber.GetPerfectnumber();
+                    break;
+
+            }
         }
     }
 }
